b2b1017b7c43940669f2e083836b0d57c804e4fa
Iryna Dzundza <irynadzundza@Irynas-Air.hsd1.il.comcast.net>
2023-11-30 12:57:11 -0600

    uploded regression test

--- src/test/java/steps/SmokeSteps.java
+++ src/test/java/steps/SmokeSteps.java
@@ -8,8 +8,23 @@ public class SmokeSteps {
     public void i_print_hello_word() {
         System.out.println("Hello Word!");
     }
+
     @Then("everybody is happy")
     public void everybody_is_happy() {
         System.out.println("We are happy!");
+    }
+
+    @Given("I run cucumber test")
+    public void i_run_cucumber_test() {
+        for(int i = 0; i < 100; i ++ ){
+            System.out.println("This is Cucumber Test!!!");
+        }
+    }
+
+    @Then("verify this is Cucumber")
+    public void verify_this_is_cucumber() {
+        for(int i = 0; i < 20; i ++ ){
+            System.out.println("Indeed this Test!!!");
+        }
     }
 }
